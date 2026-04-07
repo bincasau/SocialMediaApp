17e75d442f50ea56aaabd4ee15d4da5793e7b110
kieuphat159 <kieuphat159@gmail.com>
2026-04-07 20:31:36 +0700

    feat: fetch posts

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('setting', views.setting, name='setting'),
     path('upload', views.upload, name='upload'),
     path('signup', views.signup, name='signup'),
+    path('like-post', views.like_post, name='like-post'),
+    path('profile/<str:username>/', views.profile, name='profile'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
 ]
